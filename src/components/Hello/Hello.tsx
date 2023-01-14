7ee99765e64ad76f1054ed93802d5d3a48fe3c7f
hunghg255 <giahunghust@gmail.com>
2023-01-15 00:30:35 +0700

    init

---
+++ src/components/Hello/Hello.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+const Hello = () => {
+  return (
+    <>
+      <h1 className={styles.title}>Hello Hello</h1>
+    </>
+  );
+};
+
+export default Hello;
